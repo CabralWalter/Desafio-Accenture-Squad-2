58398dfdb0ed421436e41bccb03e59686e38370d
igor <diegoigor2001@gmail.com>
2022-09-30 14:05:36 -0300

    criando validação do formulario de acesso

---
+++ Projeto/frontend/FormulárioDeAcesso/script.js
@@ -0,0 +1,12 @@
+function logar(){
+    //capturando os valores dos input's 
+    let login = document.getElementById('usuario').value;
+    let senha = document.getElementById('senha').value;
+
+    //validação do usuario e senha "nessa validação estamos trabalhando com um unico usuario e senha"
+    if(login == 'admin' && senha == 'admin'){
+        location.href = "home.html"
+    }else{
+        alert('Usuario ou senha invalido');
+    }
+}
