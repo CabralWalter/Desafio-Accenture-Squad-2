dea620298a4f39436f2106d9410816a40947ed9d
igor <diegoigor2001@gmail.com>
2022-10-17 15:28:59 -0300

    aplicacao angular

---
+++ Projeto/aplicacao/inicio/src/app/views/omhold/omhold.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-omhold',
+  templateUrl: './omhold.component.html',
+  styleUrls: ['./omhold.component.css']
+})
+export class OmholdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
